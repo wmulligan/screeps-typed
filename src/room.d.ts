90c89f633ad9bbc782db7361372b9e7844249771
ezolenko <zolenkoe@gmail.com>
2017-01-04 22:32:06 -0700

    - docs

--- src/room.d.ts
+++ src/room.d.ts
@@ -99,7 +99,7 @@ declare class Room {
    *     the same name (hash key). If not defined, a random name will be generated.
    * @param color The color of a new flag. Should be one of the COLOR_* constants. The default value is COLOR_WHITE.
    * @param secondaryColor The secondary color of a new flag. Should be one of the COLOR_* constants. The default value is equal to color.
-   * @returns Result Code: OK, ERR_NAME_EXISTS, ERR_INVALID_ARGS
+   * @returns Result Code: FlagName, ERR_NAME_EXISTS, ERR_INVALID_ARGS
    */
   public createFlag(x: number, y: number, name?: FlagNameOrString, color?: Color, secondaryColor?: Color): ResponseCode | FlagName;
 
@@ -113,7 +113,7 @@ declare class Room {
    *     the same name (hash key). If not defined, a random name will be generated.
    * @param color The color of a new flag. Should be one of the COLOR_* constants. The default value is COLOR_WHITE.
    * @param secondaryColor The secondary color of a new flag. Should be one of the COLOR_* constants. The default value is equal to color.
-   * @returns Result Code: OK, ERR_NAME_EXISTS, ERR_INVALID_ARGS
+   * @returns Result Code: FlagName, ERR_NAME_EXISTS, ERR_INVALID_ARGS
    */
   public createFlag(pos: RoomPosition | RoomObject, name?: FlagNameOrString, color?: Color, secondaryColor?: Color): ResponseCode | FlagName;
 
