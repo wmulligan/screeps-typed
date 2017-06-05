334aabae752ce2336478710c5211556ee82af050
ezolenko <zolenkoe@gmail.com>
2017-06-04 23:04:46 -0600

    - raw memory segments

--- src/rawMemory.d.ts
+++ src/rawMemory.d.ts
@@ -5,19 +5,30 @@ declare const RawMemory: RawMemory;
  */
 interface RawMemory {
 
-  /**
-   * Get a raw string representation of the Memory object.
-   *
-   * @returns Returns a string value.
-   */
-  get(): SerializedMemory;
+    /**
+     * An object with asynchronous memory segments available on this tick. Each object key is the segment ID with data in string values. 
+     * Use RawMemory.setActiveSegments to fetch segments on the next tick. Segments data is saved automatically in the end of the tick. 
+     */
+    segments: string[];
 
-  /**
-   * Set new memory value.
-   * @param value New memory value as a string.
-   */
-  set(value: SerializedMemory): void;
+    /**
+     * Get a raw string representation of the Memory object.
+     * 
+     * @returns Returns a string value.
+     */
+    get(): SerializedMemory;
 
+    /**
+     * Set new memory value.
+     * @param value New memory value as a string.
+     */
+    set(value: SerializedMemory): void;
+
+    /**
+     * Request memory segments using the list of their IDs. Memory segments will become available on the next tick in RawMemory.segments object.
+     * @param ids An array of segment IDs. Each ID should be a number from 0 to 99. Maximum 10 segments can be active at the same time. Subsequent calls of setActiveSegments override previous ones.
+     */
+    setActiveSegments(ids: number[]): void;
 }
 
 type SerializedMemory = string;
