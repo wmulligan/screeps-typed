1b9a250d733bd322873efbf089683ea0c4db3135
Eugene Zolenko <zolenkoe@gmail.com>
2017-10-15 11:38:40 -0600

    - updating RawMemory (#7)

--- src/rawMemory.d.ts
+++ src/rawMemory.d.ts
@@ -6,14 +6,15 @@ declare const RawMemory: RawMemory;
 interface RawMemory {
 
     /**
-     * An object with asynchronous memory segments available on this tick. Each object key is the segment ID with data in string values. 
-     * Use RawMemory.setActiveSegments to fetch segments on the next tick. Segments data is saved automatically in the end of the tick. 
+     * An object with asynchronous memory segments available on this tick. Each object key is the segment ID with data in string values.
+     * Use RawMemory.setActiveSegments to fetch segments on the next tick. Segments data is saved automatically in the end of the tick.
      */
-    segments: string[];
+    segments: { [id: number]: string };
 
+    foreignSegment: ForeignMemorySegment;
     /**
      * Get a raw string representation of the Memory object.
-     * 
+     *
      * @returns Returns a string value.
      */
     get(): SerializedMemory;
@@ -29,6 +30,32 @@ interface RawMemory {
      * @param ids An array of segment IDs. Each ID should be a number from 0 to 99. Maximum 10 segments can be active at the same time. Subsequent calls of setActiveSegments override previous ones.
      */
     setActiveSegments(ids: number[]): void;
+    /**
+     * Request a memory segment of another user.
+     * The segment should be marked by its owner as public using setPublicSegments.
+     * The segment data will become available on the next tick in foreignSegment object.
+     * You can only have access to one foreign segment at the same time.
+     * @param {(string | null)} username The name of another user. Pass null to clear the foreign segment.
+     * @param {number} [id] The ID of the requested segment from 0 to 99. If undefined, the user's default public segment is requested as set by setDefaultPublicSegment.
+     */
+    setActiveForeignSegment(username: string | null, id?: number): void;
+    /**
+     * Set the specified segment as your default public segment.
+     * It will be returned if no id parameter is passed to setActiveForeignSegment by another user.
+     * @param {(number | null)} id The ID of the memory segment from 0 to 99. Pass null to remove your default public segment.
+     */
+    setDefaultPublicSegment(id: number | null): void;
+    /**
+     * Set specified segments as public. Other users will be able to request access to them using setActiveForeignSegment.
+     * @param {number[]} ids An array of segment IDs. Each ID should be a number from 0 to 99. Subsequent calls of setPublicSegments override previous ones.
+     */
+    setPublicSegments(ids: number[]): void;
 }
 
 type SerializedMemory = string;
+
+interface ForeignMemorySegment {
+  username: string,
+  id: number,
+  data: string
+}
